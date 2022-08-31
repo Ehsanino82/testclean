dc0674cafe7adec6840d1244320b8994df7bf44b
Ehsanino82 <ehsan8202@gmail.com>
2022-08-31 17:46:10 +0430

    clean

--- views.py
+++ views.py
@@ -1,5 +1,28 @@
 @login_required
 @transaction.commit_on_success
+def get_params(transaction, currency, policy):
+    return {
+        "payment_amount":
+            decimal.Decimal(transaction.amount).quantize(
+                decimal.Decimal('0.00000001'),
+                rounding=decimal.ROUND_DOWN).normalize(),
+        "payment_address":
+            transaction.address,
+        "payment_qr":
+            transaction.qrcode_url,
+        "gateway_status":
+            transaction.status_url,
+        "policy_cover":
+            policy.cover,
+        "exchange_name":
+            policy.exchange.name,
+        "date_of_formatting":
+            policy.request_date.date(),
+        "currency":
+            currency
+    }
+
+
 def create_w(request, id):
     if Register.objects.filter(id=id).exists() and Register.objects.get(id=id).sender == request.sender:
         return create_response([id], user_friendly=True)
@@ -45,7 +68,6 @@ def create_payment(request):
         policy = InsurancePolicy.objects.get(id=policy_id)
         try:
             payment = policy.payment_id
-            payment.id
         except Exception as e:
             post_params = {
                 'amount': policy.fee,
@@ -65,8 +87,8 @@ def create_payment(request):
             except Exception as e:
                 logger.error(e)
                 message = 'Payment gateway is down'
-                responseData = {'error': True, 'message': message}
-                return JsonResponse(responseData)
+                response_data = {'error': True, 'message': message}
+                return JsonResponse(response_data)
 
             try:
                 try:
@@ -92,11 +114,11 @@ def create_payment(request):
 
                 except Exception as e:
                     logger.error(e)
-                    responseData = {
+                    response_data = {
                         'error': True,
                         'message': 'Payment Gateway Error'
                     }
-                    return JsonResponse(responseData)
+                    return JsonResponse(response_data)
                 else:
                     payment.save()
                     policy.payment_id = payment
@@ -112,26 +134,7 @@ def create_payment(request):
                 logger.error(e)
                 return response
             else:
-                post_params = {
-                    "payment_amount":
-                        decimal.Decimal(transaction.amount).quantize(
-                            decimal.Decimal('0.00000001'),
-                            rounding=decimal.ROUND_DOWN).normalize(),
-                    "payment_address":
-                        transaction.address,
-                    "payment_qr":
-                        transaction.qrcode_url,
-                    "gateway_status":
-                        transaction.status_url,
-                    "policy_cover":
-                        policy.cover,
-                    "exchange_name":
-                        policy.exchange.name,
-                    "date_of_formating":
-                        policy.request_date.date(),
-                    "currency":
-                        currency
-                }
+                post_params = get_params(transaction, currency, policy)
 
                 response = JsonResponse(post_params)
                 return response
@@ -154,8 +157,8 @@ def create_payment(request):
                 except Exception as e:
                     logger.error(e)
                     message = 'Payment gateway is down'
-                    responseData = {'error': True, 'message': message}
-                    return JsonResponse(responseData)
+                    response_data = {'error': True, 'message': message}
+                    return JsonResponse(response_data)
 
                 try:
                     payment = UserPayments(
@@ -192,35 +195,9 @@ def create_payment(request):
                     logger.error(e)
                     return response
                 else:
-                    post_params = {
-                        "payment_amount":
-                            decimal.Decimal(transaction.amount).quantize(
-                                decimal.Decimal('0.00000001'),
-                                rounding=decimal.ROUND_DOWN).normalize(),
-                        "payment_address":
-                            transaction.address,
-                        "payment_qr":
-                            transaction.qrcode_url,
-                        "gateway_status":
-                            transaction.status_url,
-                        "policy_cover":
-                            policy.cover,
-                        "exchange_name":
-                            policy.exchange.name,
-                        "date_of_formating":
-                            policy.request_date.date(),
-                        "currency":
-                            currency
-                    }
+                    post_params =
 
                     response = JsonResponse(post_params)
-                    return response
-
-                    message = "Payment Exist"
-                    response = JsonResponse({
-                        'status': 'false',
-                        'message': message
-                    })
                     return response
             elif payment.status == PaymentStatus.PENDING:
                 logger.info('status Pending, do nothing')
@@ -228,26 +205,7 @@ def create_payment(request):
             elif payment.status == PaymentStatus.SUCCESS:
                 logger.info('status Success')
                 transaction = policy.payment_id
-            post_params = {
-                "payment_amount":
-                    decimal.Decimal(transaction.amount).quantize(
-                        decimal.Decimal('0.00000001'),
-                        rounding=decimal.ROUND_DOWN).normalize(),
-                "payment_address":
-                    transaction.address,
-                "payment_qr":
-                    transaction.qrcode_url,
-                "gateway_status":
-                    transaction.status_url,
-                "policy_cover":
-                    policy.cover,
-                "exchange_name":
-                    policy.exchange.name,
-                "date_of_formating":
-                    policy.request_date.date(),
-                "currency":
-                    currency
-            }
+            post_params = get_params(transaction, currency, policy)
 
             response = JsonResponse(post_params)
             return response
@@ -301,14 +259,10 @@ def backup_to_csv(request):
     insurance_report = cursor.fetchall()
 
     if request.method == 'GET':
-        datasets = {}
-        datasets['referral'] = not bool(request.GET.get('referral'))
-        datasets['user'] = not bool(request.GET.get('user'))
-        datasets['exchange'] = not bool(request.GET.get('exchange'))
-        datasets['payments'] = not bool(request.GET.get('payments'))
-        datasets['policy'] = not bool(request.GET.get('policy'))
-        datasets['case'] = not bool(request.GET.get('case'))
-        datasets['additional'] = not bool(request.GET.get('additional'))
+        datasets = {'referral': not bool(request.GET.get('referral')), 'user': not bool(request.GET.get('user')),
+                    'exchange': not bool(request.GET.get('exchange')),
+                    'payments': not bool(request.GET.get('payments')), 'policy': not bool(request.GET.get('policy')),
+                    'case': not bool(request.GET.get('case')), 'additional': not bool(request.GET.get('additional'))}
         response = HttpResponse(content_type='application/zip')
         response['Content-Disposition'] = 'attachment; filename=backup.csv.zip'
         z = zipfile.ZipFile(response, 'w')
@@ -359,21 +313,21 @@ def dashboard(request):
     )
 
     try:
-        userPartner = Partner.objects.get(
+        user_partner = Partner.objects.get(
             django_user=request.user.id)
         logger.info(
-            "Partner: %s logged into system" % (userPartner))
+            "Partner: %s logged into system" % user_partner)
         return account(request)
     except ObjectDoesNotExist:
         pass
 
-    insurancy_policy_info = Policy.objects.order_by('-id').filter(
+    insurance_policy_info = Policy.objects.order_by('-id').filter(
         user=user.id).exclude(status=PolicyStatus.DELETED)
-    DEFAULT_VALUE = "NOT FOUND"
-    PERIOD_NOT_IDENTIFIED = "NOT SET UNTIL PAYMENT DONE"
-    PAYMENT_ERROR = "REPEAT PAYMENT"
+    default_value = "NOT FOUND"
+    period_not_identified = "NOT SET UNTIL PAYMENT DONE"
+    payment_error = "REPEAT PAYMENT"
     try:
-        found_fee_values = insurancy_policy_info.values('fee')
+        found_fee_values = insurance_policy_info.values('fee')
     except Exception:
         logger.error("Fee values hasn't been found for user with ID: " +
                      str(user.id))
@@ -385,7 +339,7 @@ def dashboard(request):
 
     policy_numbers = []
     try:
-        found_policy_numbers = insurancy_policy_info.values('id')
+        found_policy_numbers = insurance_policy_info.values('id')
     except KeyError as error:
         logger.error("Policy number hasn't been found for user with ID: " +
                      str(user.id))
@@ -394,9 +348,8 @@ def dashboard(request):
     for current_policy_number_json in found_policy_numbers:
         policy_numbers.append(current_policy_number_json)
 
-
     try:
-        found_start_dates = insurancy_policy_info.values('start_date')
+        found_start_dates = insurance_policy_info.values('start_date')
     except KeyError as error:
         logger.error("Couldn't find start dates for user with ID: " +
                      str(user.id))
@@ -407,7 +360,7 @@ def dashboard(request):
         start_dates.append(current_date)
 
     try:
-        found_expiration_dates = insurancy_policy_info.values(
+        found_expiration_dates = insurance_policy_info.values(
             'expiration_date')
     except KeyError as error:
         logger.error("Couldn't find expirations dates for user with ID: " +
@@ -419,7 +372,7 @@ def dashboard(request):
         expiration_dates.append(current_date)
 
     try:
-        found_limits_of_liability = insurancy_policy_info.values('cover_btc')
+        found_limits_of_liability = insurance_policy_info.values('cover_btc')
     except KeyError as error:
         logger.error("Couldn't find limits of liability for user with ID: " +
                      str(user.id))
@@ -430,7 +383,7 @@ def dashboard(request):
         limits_of_liability.append(current_limit)
 
     try:
-        found_dates_of_formatting = insurancy_policy_info.values(
+        found_dates_of_formatting = insurance_policy_info.values(
             'request_date')
     except KeyError as error:
         logger.error("Couldn't find dates of formatting for user with ID: " +
@@ -442,7 +395,7 @@ def dashboard(request):
         dates_of_formatting.append(current_date_of_formatting)
 
     try:
-        found_stock_exchanges = insurancy_policy_info.values('exchange')
+        found_stock_exchanges = insurance_policy_info.values('exchange')
     except KeyError as error:
         logger.error("Couldn't find stock exchanges for user with ID: " +
                      str(user.id))
@@ -453,7 +406,7 @@ def dashboard(request):
         stock_exchange_ids.append(current_stock_exchange_id)
 
     try:
-        found_policy_statuses = insurancy_policy_info.values('status')
+        found_policy_statuses = insurance_policy_info.values('status')
     except KeyError as error:
         logger.error("Couldn't find policy statuses for user with ID: " +
                      str(user.id))
@@ -463,15 +416,15 @@ def dashboard(request):
     for policy_status in found_policy_statuses:
         policy_statuses.append(policy_status)
 
-    contextPolicy = []
-    for current_id, policy_id in enumerate(insurancy_policy_info):
+    context_policy = []
+    for current_id, policy_id in enumerate(insurance_policy_info):
 
-        context_policy_number = DEFAULT_VALUE
-        context_limit = DEFAULT_VALUE
-        context_date_of_formatting = DEFAULT_VALUE
-        context_insurance_period = PERIOD_NOT_IDENTIFIED
-        context_fee = DEFAULT_VALUE
-        context_stock_exchange = DEFAULT_VALUE
+        context_policy_number = default_value
+        context_limit = default_value
+        context_date_of_formatting = default_value
+        context_insurance_period = period_not_identified
+        context_fee = default_value
+        context_stock_exchange = default_value
 
         policy_number_tag = "Crypto"
         try:
@@ -479,14 +432,14 @@ def dashboard(request):
                                     str((policy_numbers[current_id])['id'])
         except (IndexError, KeyError) as error:
             logger.error(
-                "An error has occured while trying to get policy number.\
+                "An error has occurred while trying to get policy number.\
                 Reason: " + str(error))
 
         try:
             context_fee = fee_values[current_id]['fee']
         except (IndexError, KeyError) as error:
             logger.error(
-                "An error has occured while trying to get fee. Reason: " +
+                "An error has occurred while trying to get fee. Reason: " +
                 str(error))
 
         try:
@@ -497,16 +450,16 @@ def dashboard(request):
                 e_date.day, e_date.strftime("%B")[0:3], e_date.year - 2000)
         except (IndexError, KeyError, AttributeError) as error:
             if policy_id.payment_id and policy_id.payment_id.status < 0:
-                context_insurance_period = PAYMENT_ERROR
+                context_insurance_period = payment_error
                 logger.error(
-                    "An error has occured while trying to get insurane period.\
+                    "An error has occurred while trying to get insurance period.\
                     Reason: " + str(error))
 
         try:
             context_limit = limits_of_liability[current_id]['cover_btc']
         except (IndexError, KeyError) as error:
             logger.error(
-                "An error has occured while trying to get limit . Reason: " +
+                "An error has occurred while trying to get limit . Reason: " +
                 str(error))
 
         try:
@@ -514,7 +467,7 @@ def dashboard(request):
                 dates_of_formatting[current_id]['request_date'].date())
         except (IndexError, KeyError, AttributeError) as error:
             logger.error(
-                "An error has occured while trying to get date of formatting.\
+                "An error has occurred while trying to get date of formatting.\
                 Reason: " + str(error))
 
         try:
@@ -523,9 +476,8 @@ def dashboard(request):
             context_stock_exchange = exchange_tag['name']
         except (IndexError, KeyError) as error:
             logger.error(
-                "An error has occured while trying to get exchange tag.\
+                "An error has occurred while trying to get exchange tag.\
                 Reason: " + str(error))
-
 
         try:
             policy_status_numerical_value = policy_statuses[current_id][
@@ -534,19 +486,19 @@ def dashboard(request):
                 policy_status_numerical_value)
         except (IndexError, KeyError) as error:
             logger.error(
-                "An error has occured while trying to get policy status.\
+                "An error has occurred while trying to get policy status.\
                 Reason: " + str(error))
 
         sos = False
         try:
-            sosexists = InsuranceCase.objects.filter(
+            sos_exists = InsuranceCase.objects.filter(
                 insurance=(policy_numbers[current_id])['id'])
-            logger.debug(sosexists.count())
-            if sosexists.count() > 0:
+            logger.debug(sos_exists.count())
+            if sos_exists.count() > 0:
                 sos = True
         except (IndexError, KeyError) as error:
             logger.error(
-                "An error has occured while trying to get InsuranceCase.\
+                "An error has occurred while trying to get InsuranceCase.\
                 Reason: " + str(error))
         if start_dates[current_id]['start_date']:
             days = expiration_dates[current_id]['expiration_date'] - \
@@ -583,9 +535,9 @@ def dashboard(request):
                     rounding=decimal.ROUND_DOWN).normalize(),
             'status':
                 policy_status_tag,
-            'numstatus':
+            'num_status':
                 policy_status_numerical_value,
-            'sosexists':
+            'sos_exists':
                 sos,
             'expired_soon':
                 expired_soon,
@@ -593,7 +545,7 @@ def dashboard(request):
                 days_left
         }
         logger.debug(context_policies)
-        contextPolicy.append(context_policies)
+        context_policy.append(context_policies)
 
     stock_exchange_tags = set()
     for stock_exchange in stock_exchange_ids:
@@ -607,10 +559,10 @@ def dashboard(request):
             name=stock_exchange).values('coverage_limit')[0])['coverage_limit']
         current_stock_exchange = stock_exchange
         amount_of_holdings = 0
-        for policy in contextPolicy:
+        for policy in context_policy:
             if policy['stock'] == current_stock_exchange and (
-                    policy['numstatus'] == 1
-                    or policy['numstatus'] == 2):
+                    policy['num_status'] == 1
+                    or policy['num_status'] == 2):
                 amount_of_holdings += float(policy['limit'])
         user_limit_information = {
             'stock_exchange': current_stock_exchange,
@@ -619,10 +571,10 @@ def dashboard(request):
             'rate': int(amount_of_holdings / float(coverage_limit) * 100)
         }
         user_limit_information_context.append(user_limit_information)
-        logger.debug(contextPolicy)
+        logger.debug(context_policy)
     context = {
         'USER_LIMIT_INFO': user_limit_information_context,
-        'POLICIES': contextPolicy,
+        'POLICIES': context_policy,
         'is_referral': False
     }
     return render(request, 'website/dashboard/dashboard.html', context)
